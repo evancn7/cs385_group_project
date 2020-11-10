8deb54c9ff9174b11243be7b467308590cd323dc
Evan Campion <evancn7@gmail.com>
2020-11-10 19:57:18 +0000

    modified the home page to include all members of the team

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    let members = ["Chris Boland", "Pavel", "Cristina Glez", "Evan Campion"];
+    return (
+      <div className="App">
+        <h1>CS385 Group Project</h1>
+        <ul>
+          <li>{members[0]}</li>
+          <li>{members[1]}</li>
+          <li>{members[2]}</li>
+          <li>{members[3]}</li>
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
