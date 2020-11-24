626ed5c8e2661266ddcb66ee9f5ab0cc4b181e3a
Evan Campion <evancn7@gmail.com>
2020-11-24 11:55:17 +0000

    add bootstrap to html, replace template with user login, update css

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    let members = ["Chris Boland", "Pavel", "Cristina Glez", "Evan Campion"];
+    // let members = ["Chris Boland", "Pavel", "Cristina Glez", "Evan Campion"];
     return (
       <div className="App">
-        <h1>CS385 Group Project</h1>
-        <ul>
-          <li>{members[0]}</li>
-          <li>{members[1]}</li>
-          <li>{members[2]}</li>
-          <li>{members[3]}</li>
-        </ul>
+        <div class="container">
+          <div class="app-logo"><h1>Team Nickel</h1></div>
+          <div class="log-in-container">
+            <h3>Log In</h3>
+            <form action="action_page.php" method="post">
+              <div class="form-container">
+                <div class="user-input-container">
+                  <label for="uname"><b>Username</b></label>
+                  <input type="text" placeholder="Enter Username" name="uname" required></input>
+                  <label for="psw"><b>Password</b></label>
+                  <input type="password" placeholder="Enter Password" name="psw" required></input>
+                </div>
+                <div class="log-in-button-container">
+                  <div id="log-in-button">
+                    <button type="submit">Login</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="new-account-container">
+            <button>Create New Account</button>
+          </div>
+        </div>
       </div>
     );
   }
