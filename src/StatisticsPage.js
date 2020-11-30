3aa9b25dcc2e8f01d910ec65fc1d7320ece79532
Evan Campion <evancn7@gmail.com>
2020-11-30 15:00:55 +0000

    amend className to StatisticsPage, add more cards and remove unused card

--- src/StatisticsPage.js
+++ src/StatisticsPage.js
@@ -4,8 +4,7 @@ import React, { Component } from "react";
 class StatisticsPage extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div className="StatisticsPage">
 
         <div class="card" class="grey-bg container-fluid">
           <div class="card-content">
@@ -23,12 +22,83 @@ class StatisticsPage extends Component {
           </div>
         </div>
 
-        <div class="card" class="container-fluid">
-          <div class="card-body">
-            <h5 class="card-title">Carbon Footprint</h5>
-            <h6 class="card-subtitle mb-2 text-muted">What is your Carbon Footprint?</h6>
-            <p class="card-text">High</p>
-            <a href="#" class="card-link">Full Stats</a>
+        <div class="card" class="grey-bg container-fluid">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <h3 class="font-large-2 float-left">Carbon</h3>
+                </div>
+                <div class="media-body text-right">
+                  <h3>High</h3>
+                  <span>Carbon Emissions</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card" class="grey-bg container-fluid">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <h3 class="font-large-2 float-left">Carbon</h3>
+                </div>
+                <div class="media-body text-right">
+                  <h3>High</h3>
+                  <span>Carbon Emissions</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card" class="grey-bg container-fluid">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <h3 class="font-large-2 float-left">Carbon</h3>
+                </div>
+                <div class="media-body text-right">
+                  <h3>High</h3>
+                  <span>Carbon Emissions</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card" class="grey-bg container-fluid">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <h3 class="font-large-2 float-left">Carbon</h3>
+                </div>
+                <div class="media-body text-right">
+                  <h3>High</h3>
+                  <span>Carbon Emissions</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card" class="grey-bg container-fluid">
+          <div class="card-content">
+            <div class="card-body">
+              <div class="media d-flex">
+                <div class="align-self-center">
+                  <h3 class="font-large-2 float-left">Carbon</h3>
+                </div>
+                <div class="media-body text-right">
+                  <h3>High</h3>
+                  <span>Carbon Emissions</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
