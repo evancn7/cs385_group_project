fb38255ccce2a00197ecdc8e6a05371c7ae2cb65
Evan Campion <evancn7@gmail.com>
2020-11-30 14:52:06 +0000

    add imports for other components, to change page view in nav bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 // import components
 import LandingPage from "./LandingPage";
 import HomePage from "./HomePage";
+import BasketPage from "./BasketPage";
+import StatisticsPage from "./StatisticsPage";
 import "./css/App.css";
 
 
