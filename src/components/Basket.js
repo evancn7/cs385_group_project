e87f678ada72fe34f3fa0ad972c09dec00659067
chrissb7 <chrissbb90@gmail.com>
2020-12-05 14:46:37 +0000

    Basket Changes pt.2

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,75 +1,48 @@
 import React, { Component } from "react";
 
-
 class Basket extends Component {
-
-  getCalculatedPrice(acc, obj) {
-    return acc + obj.price;
+  totalCalories(acc, obj) {
+    return acc + obj.cals;
   }
-
-  //place sum of functions here
-
+  totalCarbon(acc, obj) {
+    return acc + obj.carbon;
+  }
   render() {
-
-    const carbonProps = this.props.CarbonFootPrintArray;
-    const caloriesProps = this.props.CaloriesArray;
-    const costProps = this.props.CostArray;
-    const productProps= this.props.ProductArray;
-
-    // we simply use reduce on all three arrays which have been
-    // passed via this.props.
-    const totalCost = costProps.reduce((this.getCalculatedPrice, 0.0));
-
-    const totalCarbonfootprint = carbonProps.reduce((accumulator, currentvalue)=>accumulator+currentvalue);
-
-    const totalCalories = caloriesProps.reduce((accumulator, currentvalue)=>accumulator+currentvalue);
-    
-    //reformat getCalculatedPrice for sum of calories and carbonfootprint
-
-    
-
+    const userList = this.props.userList;
     return (
-      <div className="alert alert-dark" role="alert">
-        <h2>Your Total Basket Cost</h2>
-        <p className="lead">TOTAL COST: €{totalCost.toFixed(2)}</p>
+      <div className="Basket">
+        <table className="table table-sm table-bordered container-fluid">
+          <thead className="thead-light">
+            <tr>
+              <th>Name</th>
+              <th>Cals</th>
+              <th>Protein</th>
+              <th>Fat</th>
+              <th>Carbon</th>
+              <th>Qty</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map((a) => (
+              <tr key={a.foodID}>
+                <td>{a.name}</td>
+                <td>{a.cals}</td>
+                <td>{a.protein}</td>
+                <td>{a.fat}</td>
+                <td>{a.carbon}</td>
+                <td></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-        {productProps.length <= 0 && (
-            <p className="lead text-justify">
-              ORDER INCOMPLETE: You'll need to add a product to start your
-              order.
-            </p>
-          )}
-
+        <div>Total Calories: {userList.reduce(this.totalCalories, 0)}</div>
         
-        <div className="lead">
-        {totalCarbonfootprint>80 && <span> Carbon Footprint: High </span>}
-        {totalCarbonfootprint<50 && <span> Carbon Footprint: Medium </span>}
-        {totalCarbonfootprint>80 && <span> Carbon Footprint: Low </span>}
-        </div>
-        
-        
-        
-        {/* /* <ul className="lead">
-          {caloriesProps.map((s, index) => (
-            <li key={index}>
-              : <b>{s.topName}</b>, €
-              {(custSizeChoice * s.price).toFixed(2)}
-            </li>
-          ))}
-          </ul> */ }
+        <div>Total Carbon: {userList.reduce(this.totalCarbon, 0)}</div>
 
-
-       
-
-        {productProps.length > 0 &&
-          <div className="lead text-justify">All done?
-          <button type="button" class="btn btn-warning btn-lg btn-block">Place My Order</button>
-      </div>}
       </div>
     );
-  } // render
-} // class Basket
+  }
+}
 
 export default Basket;
-
-
