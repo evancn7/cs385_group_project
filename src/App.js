a3fa23ea5d750459ef01aefa8e2b2db595ca92ff
Evan Campion <evancn7@gmail.com>
2020-12-09 08:52:10 +0000

    comment code on functions, sidebar animation and conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import StatisticsPage from "./pages/StatisticsPage";
+// import styles
 import "./css/App.css";
 import "./css/NavBar.css";
 
@@ -10,6 +11,7 @@ import "./css/NavBar.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    // set two state variable to handle user current page and sidebar toggle
     this.state = {
       currentPageSelection: "Home",
       sidebar: false
@@ -19,13 +21,16 @@ class App extends Component {
     this.statisticsPageClick = this.statisticsPageClick.bind(this);
     this.homeButtonClick = this.homeButtonClick.bind(this);
   }
+  // function that toggles true/false on state to animate sliding sidebar
   setSideBar() {
     this.setState({ sidebar : !this.state.sidebar });
   }
+  // update state to display HomePage, hide side bar on button click
   homeButtonClick() {
     this.setState({ currentPageSelection: "Home" });
     this.setSideBar();
   }
+  // update state to display StatisticsPage, hide side bar on button click
   statisticsPageClick() {
     this.setState({ currentPageSelection: "Statistics" });
     this.setSideBar();
@@ -36,12 +41,14 @@ class App extends Component {
         <div className="NavBar">
           <nav id="bar">
             <div className="burger" onClick={this.setSideBar}>
+              {/* animate burger on click by change of class depending on state */}
               <div className={this.state.sidebar ? "top-active" : "top"}></div>
               <div className={this.state.sidebar ? "bottom-active" : "bottom"}></div>
             </div>
             <div className="page-title">
               <h3>{this.state.currentPageSelection}</h3>
             </div>
+            {/* ? operator used to toggle the sliding sidebar, depending on state class changed */}
             <ul className={this.state.sidebar ? "nav-links-active" : "nav-links"}>
               <li className={this.state.currentPageSelection === "Home" ? "active" : ""} onClick={this.homeButtonClick}>Home</li>
               <li className={this.state.currentPageSelection === "Statistics" ? "active" : ""} onClick={this.statisticsPageClick}>Statistics</li>
@@ -49,6 +56,7 @@ class App extends Component {
           </nav>
         </div>
 
+        {/* conditional rendering based on state, button handler functions change state and change view */}
         {this.state.currentPageSelection === "Home" && <HomePage />}
         {this.state.currentPageSelection === "Statistics" && <StatisticsPage />}
 
