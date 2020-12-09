c14aff75556d2acbdb5295e026ea8c78e27a0406
Evan Campion <evancn7@gmail.com>
2020-12-09 08:23:40 +0000

    update default load page to home after user login

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPageSelection: "None",
+      currentPageSelection: "Home",
       sidebar: false
     };
 
