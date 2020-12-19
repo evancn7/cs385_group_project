8f3df040fb5e3467897536cb515583c9633e67a6
Evan Campion <evancn7@gmail.com>
2020-12-19 11:34:44 +0000

    set landing page with log in log out components and inject into js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import Landing from "./views/Landing";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Landing />
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/views/Landing.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Firebase from 'firebase';
+// import components
+import Login from '../components/Login';
+import Logout from '../components/Logout';
+import App from '../App';
+
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { authenticated: false, currentUser: null };
+  }
+  componentDidMount() {
+    Firebase.auth().onAuthStateChanged((user) => {
+      user
+        ? this.setState(() => ({
+            authenticated: true,
+            currentUser: user
+          }))
+        : this.setState(() => ({
+            authenticated: false,
+            currentUser: null
+          }));
+    });
+  }
+  render() {
+    return (
+      <div className='Landing'>
+        {this.state.authenticated && <App />}
+        {this.state.authenticated && <Logout />}
+        {!this.state.authenticated && <Login />}
+      </div>
+    );
+  }
+}
+export default Landing;
