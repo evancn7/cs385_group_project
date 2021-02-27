b46e25a0be2616b1da2aa3e2a0806906a78a8682
evancn7 <evancn7@gmail.com>
2021-02-27 17:27:38 +0000

    set app to first view on url visit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./App";
-import Landing from "./views/Landing";
+import App from "./App";
+// import Landing from "./views/Landing";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Landing />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
